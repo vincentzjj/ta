37f471d8905d912790eec099e4fd5a791b5a93c7
vincentzjj <741535963@qq.com>
2017-04-16 22:00:17 +0100

    Update adcreader.h

--- adcreader.h
+++ adcreader.h
@@ -1,12 +1,3 @@
-/*
-  MCP3008.h - Library for communicating with MCP3008 Analog to digital converter.
-  Created by Uros Petrevski, Nodesign.net 2013
-  Released into the public domain.
-  
-  ported from Python code originaly written by Adafruit learning system for rPI :
-  http://learn.adafruit.com/send-raspberry-pi-data-to-cosm/python-script
-*/
-
 #ifndef adcreader_h
 #define adcreader_h
 
