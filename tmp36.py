379a805a1a0137d22bb38de345e5ac9ea0383552
vincentzjj <741535963@qq.com>
2017-04-17 00:28:28 +0100

    Update tmp36.py

--- tmp36.py
+++ tmp36.py
@@ -7,7 +7,7 @@ import datetime
 with open('./config.json') as config_file:
     plotly_user_config = json.load(config_file)
 
-py.sign_in(plotly_user_config["plotly_username"], plotly_user_config["plotly_api_key"])
+py.sign_in(plotly_user_config["zjiajun124"], plotly_user_config["XFojRosMS6fxH8IuCq9q"])
 
 url = py.plot([
     {
