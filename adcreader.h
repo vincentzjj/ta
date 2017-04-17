26b038b37c09949a0661e2b93875407ad84d0b6b
vincentzjj <741535963@qq.com>
2017-04-17 23:39:39 +0100

    Update adcreader.h

--- adcreader.h
+++ adcreader.h
@@ -1,6 +1,6 @@
 #ifndef ADCREADER
 #define ADCREADER
-
+#include "mcp3008Spi.h"
 #include <QThread>
 
 class ADCreader : public QThread
@@ -8,13 +8,15 @@ class ADCreader : public QThread
 public:
 	ADCreader() {running = 0;};
 	void quit();
-	int run(int Channel);
-	float round(float number);
-	float ConvertVolts(int adcout);
-	float ConvertTemp(int adcout);
-	float read();
+	void run();
+        float Data0();
+        float Data1();
+        float Data2();
 private:
 	bool running;
+	float output0;
+        float output1;
+        float output2;
 	
 
 };
