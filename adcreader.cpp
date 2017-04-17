c6cd647943d9a778b357497abe8ae09138a0e747
vincentzjj <741535963@qq.com>
2017-04-17 23:38:08 +0100

    Update adcreader.cpp

--- adcreader.cpp
+++ adcreader.cpp
@@ -24,40 +24,53 @@
  
 using namespace std;
 mcp3008Spi a2d("/dev/spidev0.0", SPI_MODE_0, 1000000, 8);
-int ADCreader::run(int Channel)
-{
     
-       int a2dval = 0;
-    
-        unsigned char data[3];
-    running=true;
+     int a2dval0 = 0;
+     int a2dChannel0 = 0;
+        unsigned char data0[3];
+     float voltage0 = 0;
+     int a2dval1 = 0;
+     int a2dChannel1 = 0;
+        unsigned char data0[3];
+     float voltage1 = 0;
+   void ADCreader::run()
+   {
+    running = true;
     while(running)
     {
-        data[0] = 1;  //  first byte transmitted -> start bit
-        data[1] = 0b10000000 |( ((Channel & 7) << 4)); // second byte transmitted -> (SGL/DIF = 1, D2=D1=D0=0)
-        data[2] = 0; // third byte transmitted....don't care
+        data0[0] = 1;  //  first byte transmitted -> start bit
+        data0[1] = 0b10000000 |( ((Channel & 7) << 4)); // second byte transmitted -> (SGL/DIF = 1, D2=D1=D0=0)
+        data0[2] = 0; // third byte transmitted....don't care
+     
+        data1[0] = 1;  //  first byte transmitted -> start bit
+        data1[1] = 0b10010000 |( ((Channel & 7) << 4)); // second byte transmitted -> (SGL/DIF = 1, D2=D1=D0=0)
+        data1[2] = 0; // third byte transmitted....don't care
  
-        a2d.spiWriteRead(data, sizeof(data) );
+        a2d.spiWriteRead(data0, sizeof(data0) );
  
-        a2dval = 0;
-                a2dval = (data[1]<< 8) & 0b1100000000; //merge data[1] & data[2] to get result
-                a2dval |=  (data[2] & 0xff);
-        sleep(1);
+        a2dval0 = 0;
+        a2dval0 = (data0[1]<< 8) & 0b1100000000; //merge data[1] & data[2] to get result
+        a2dval0 |=  (data0[2] & 0xff);
+        voltage0 = (a2dVal0*3.3)/float(1023);
+      
+     a2d.spiWriteRead(data1, sizeof(data1) );
+ 
+        a2dval1 = 0;
+        a2dval1 = (data1[1]<< 8) & 0b1100000000; //merge data[1] & data[2] to get result
+        a2dval1 |=  (data1[2] & 0xff);
+        voltage1 =((a2dVal1*3.3)/float(1023))*1000;
+       
     }
-    return a2dval ;
+    
 }    
 
-  float ADCreader::round(float number)
-{
-    return (number > 0.0) ? (number + 0.5) : (number - 0.5); 
-}
-
- float ADCreader::ConvertVolts(int adcout)
+ 
+ float ADCreader::Data0()
  {
-    float volts; 
-   volts = (adcout * 3.3) / float(1023);
-  volts = round(volts);
-  return volts;
+     
+   output0 = (voltage * 3.3) / float(1023);
+
+  return output0;
  }
   
   /* ADC Value
@@ -71,34 +84,12 @@ int ADCreader::run(int Channel)
     775      200    2.50
    1023      280    3.30 */
  
-float ADCreader::ConvertTemp(int adcout)
+float ADCreader::Data1()
  {
-   float temp;  
-  temp = ((adcout * 330)/float(1023))-50;
-  temp = round(temp);
-  return temp;
+  output1 = ((voltage1 * 330)/float(1023))-50;
+  return output1;
  } 
  
 
  
-float ADCreader::read()
-{
-  float temp;  
-while (1)
- {int light_channel=0;
-   int temp_channel=1;
-  
-   int light_level,temp_level;
-   float light_volts,temp_volts,temp;
-     
-   //Read the light sensor data
-  light_level = run(light_channel);
-  light_volts = ConvertVolts(light_level);
- 
-  // Read the temperature sensor data
-  temp_level = run(temp_channel);
-  temp_volts = ConvertVolts(temp_level);
-  temp       = ConvertTemp(temp_level);}
- temp=5;
- return temp;
-}
+
